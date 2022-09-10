2a3447d095ea3a92c3cf24415837714528eae6bd
yifengx <xionfyf1230@gmail.com>
2022-09-09 18:33:50 -0700

    Initial commit

---
+++ hash_collision.py
@@ -0,0 +1,36 @@
+import hashlib
+import os
+import random
+import string
+
+def hash_collision(k):
+    if not isinstance(k, int):
+        print("hash_collision expects an integer")
+        return (b'\x00', b'\x00')
+    if k < 0:
+        print("Specify a positive number of bits")
+        return (b'\x00', b'\x00')
+
+    # Collision finding code goes here
+    dic = {}
+    source = string.ascii_letters + string.digits
+    start = ""
+    m = hashlib.sha256()
+    while True:
+        char = random.choice(source)
+        start += char
+        m.update(char.encode('utf-8'))
+        new_hex = m.hexdigest()
+        last_k = get_last_k_bit(k, new_hex)
+        if last_k in dic:
+            x = start
+            y = dic.get(last_k)
+            break
+    return (x, y)
+
+
+def get_last_k_bit(k, a):
+    bit = int(a, 16)
+    mask = 1 << k
+    last_k = bit & (mask - 1)
+    return last_k
